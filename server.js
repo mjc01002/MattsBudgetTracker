2374af71352a56f3160f49dbb61278934ead5334
stoncalkins@gmail.com <stoncalkins@gmail.com>
2022-02-08 07:59:27 -0500

    updated code

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mattsBudget', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+mongoose.connect(process.env.MONGODB_URI, function (err) {
+  console.log('connected to mongodb')
 });
-//mongoose.connect(process.env.MONGODB_URI, function (err) {
-  //console.log('connected to mongodb')
-//});
 
 // routes
 app.use(require("./routes/api.js"));
